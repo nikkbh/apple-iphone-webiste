c6dfb228094ccf8785a690772aa94aa1331a66be
nikkbh <nikhilbhutani111@yahoo.com>
2023-04-09 15:03:39 +0530

    Complete Apple iPhone Website Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import DisplaySection from "./components/DisplaySection.jsx";
+import Jumbotron from "./components/Jumbotron.jsx";
+import Nav from "./components/Nv.jsx";
+import SoundSection from "./components/SoundSection.jsx";
+import WebgiViewer from "./components/WebgiViewer.jsx";
+import { useRef } from 'react';
+import Loader from "./components/Loader.jsx";
 
 function App() {
 
+  const webgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  };
+
   return (
     <div className="App">
-      Hello
+      <Loader />
+      <div id="content" ref={contentRef}>
+        <Nav />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViewer contentRef={contentRef} ref={webgiViewerRef} />
     </div>
   );
 }
